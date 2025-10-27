08837254e1bfa455cb7bfa0b211feb12587aa059
kalyan199999 <gottapupavankalyan@gmail.com>
2025-10-27 12:32:37 +0530

    web app is added!

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask , request , render_template
-import numpy as np
+from flask import Flask, request, jsonify,render_template
+import pickle
 import pandas as pd
 
-application = Flask(__name__)
-app = application
+app = Flask(__name__)
+
+model = pickle.load(open('./models/svc.pkl', 'rb'))
+preprocessor = pickle.load(open('./models/preprocessor.pkl', 'rb'))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template( 'index.html' )
 
-@app.route('/predict',methods=['GET','POST'])
-def predict_datapoint():
-    if request.method == 'GET':
-        return render_template('home.html')
-    else :
-        
-        pass
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    df = pd.DataFrame([data])
+
+    for col in df.select_dtypes(include='object').columns:
+        df[col] = df[col].str.lower().str.strip()
+            
+    X = preprocessor.transform(df)
+
+    pred = model.predict(X)
+    return jsonify({'prediction': pred[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
